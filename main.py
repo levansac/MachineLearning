0ca1a49a79dabb502c8362175aaa1588e4732d15
levansac <57362394+levansac@users.noreply.github.com>
2025-06-25 19:33:09 +0700

    commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import numpy.random as rnd
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+plt.show()
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+X[0]
